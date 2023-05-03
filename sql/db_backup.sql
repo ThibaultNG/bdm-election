1305efae9f60058683b1854281f8c92c6fd9b4af
Thibault.NG <thibault.ng@etu.u-pec.fr>
2023-05-03 15:53:04 +0200

    fix db_backup.sql

--- sql/db_backup.sql
+++ sql/db_backup.sql
@@ -1,5 +1,3 @@
-
-
 -- drop all tables
 DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
@@ -14,48 +12,53 @@ CREATE TABLE state (
     id_state SERIAL PRIMARY KEY,
     state_name VARCHAR(255),
     po_code VARCHAR(2),
-    fips_code INTEGER,
+    fips_code VARCHAR(2),
     cen_code INTEGER,
     ic_code INTEGER
 );
 
 CREATE TABLE district (
     id_district SERIAL PRIMARY KEY,
-    number_district INTEGER,
-    id_state INTEGER NOT NULL REFERENCES state(id_state)
+    district_number VARCHAR(2),
+    id_state INTEGER,
+    CONSTRAINT fk_district_state FOREIGN KEY (id_state) REFERENCES state(id_state)
 );
 
 CREATE TABLE year (
     id_year SERIAL PRIMARY KEY,
-    year INTEGER
+    year_label INTEGER
 );
 
 CREATE TABLE party (
     id_party SERIAL PRIMARY KEY,
-    name_party VARCHAR(255)
+    party_name VARCHAR(255)
 );
 
 CREATE TABLE person (
     id_person SERIAL PRIMARY KEY,
-    name_person VARCHAR(255)
+    person_name VARCHAR(255)
 );
 
 CREATE TABLE candidate (
     id_candidate SERIAL PRIMARY KEY,
-    write_in_candidate BOOLEAN,
     fusion_ticket_candidate BOOLEAN,
-    id_party INTEGER NOT NULL REFERENCES party(id_party),
-    id_person INTEGER NOT NULL REFERENCES person(id_person)
+    id_party INTEGER,
+    id_person INTEGER,
+    CONSTRAINT fk_candidate_party FOREIGN KEY (id_party) REFERENCES party(id_party),
+    CONSTRAINT fk_candidate_person FOREIGN KEY (id_person) REFERENCES person(id_person)
 );
 
 -- factual tables
 CREATE TABLE vote_fact (
-    id_district INTEGER NOT NULL REFERENCES district(id_district),
-    id_year INTEGER NOT NULL REFERENCES year(id_year),
-    id_candidate INTEGER NOT NULL REFERENCES candidate(id_candidate),
+    id_district INTEGER,
+    id_year INTEGER,
+    id_candidate INTEGER,
     candidate_vote INTEGER,
     total_vote INTEGER,
-    run_off_vote BOOLEAN,
-    special_vote BOOLEAN,
-    PRIMARY KEY (id_district, id_year, id_candidate)
+    run_off_election BOOLEAN,
+    special_election BOOLEAN,
+    PRIMARY KEY (id_district, id_year, id_candidate),
+    CONSTRAINT fk_vote_district FOREIGN KEY (id_district) REFERENCES district(id_district),
+    CONSTRAINT fk_vote_year FOREIGN KEY (id_year) REFERENCES year(id_year),
+    CONSTRAINT fk_vote_candidate FOREIGN KEY (id_candidate) REFERENCES candidate(id_candidate)
 );
